bd32153534bb92c11d8b9e3b27f464104a4b8b27
OGDuper <zhvnndos@gmail.com>
2024-12-31 16:18:50 +0500

    Update roblox-proxy.js

--- roblox-proxy.js
+++ roblox-proxy.js
@@ -2,6 +2,14 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/api/avatar', async (req, res) => {
     const { nickname } = req.query;
 
